bcccf637797405fb1a8e94d39fbe447f0a869218
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-11-21 21:05:46 -0300

    add correction the bug in the cod

--- script/cards.js
+++ script/cards.js
@@ -1,6 +1,4 @@
 const playerName = document.querySelector(".playerName");
 const storagePlayerName = localStorage.getItem("@memoryGame:playerName");
 
-console.log(storagePlayerName);
-console.log(playerName.innerHTML);
 playerName.innerHTML = storagePlayerName;
